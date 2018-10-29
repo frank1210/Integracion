db75cf205c965b72df4f07a371062f5c45443bb4
Franco <francolopez_16@hotmail.com>
2018-10-29 15:29:00 -0300

    List facturas

--- Integracion/src/bean/ListFacturas.java
+++ Integracion/src/bean/ListFacturas.java
@@ -4,6 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListFacturas {
-	private List<Factura> facturas =new ArrayList<Factura>();
+	private List<Factura> Facturas =new ArrayList<Factura>();
+	
+	public List<Factura> getFacturas() {
+		return Facturas;
+	}
 
+	public void setFacturas(List<Factura> Facturas) {
+		this.Facturas = Facturas;
+	}
+	
+	public void addFactura(Factura p){
+		
+		Facturas.add(p);
+	}
+	
+	public void removeFactura(Factura p){
+		Facturas.remove(p);
+	}
+	
+	public Factura getPersonaPorDni(int codigo){
+		for(Factura p : Facturas){
+			if(p.getNumero()==codigo) return p;
+		}
+		return null;
+		
+	}
+	
 }
