f5b9b54694c0aac51ceccf21d8d6a1de923ce152
Franco <francolopez_16@hotmail.com>
2018-10-29 15:17:58 -0300

    Abono + srv+dao

--- Integracion/src/bean/dao/HibernateAbonoDAO.java
+++ Integracion/src/bean/dao/HibernateAbonoDAO.java
@@ -1,5 +1,66 @@
 package bean.dao;
 
+import hbt.HibernateUtil;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import bean.Abono;
+import bean.Factura;
+
 public class HibernateAbonoDAO {
+
+	private static HibernateAbonoDAO instancia = null;
+	private static SessionFactory sf = null;
+
+	public static HibernateAbonoDAO getInstancia(){
+		if(instancia == null){
+			sf = HibernateUtil.getSessionFactory();
+			instancia = new HibernateAbonoDAO();
+		} 
+		return instancia;
+	}
+	
+	public void grabarAbonos(List<Abono> Abonos){
+		Session session = sf.openSession();
+		session.beginTransaction();
+		for(Abono f:Abonos)
+			session.merge(f);
+		session.flush();
+		session.getTransaction().commit();
+		session.close();
+	}
+	
+	public Abono seleccionarAbono(int codigo){
+		Abono f = new Abono();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		f=session.get(Abono.class, codigo);
+		session.flush();
+		session.getTransaction().commit();
+		session.close();
+		
+		return f;
+	}
+	
+	public void borrarAbono(Abono f){
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.delete(f);
+		session.flush();
+		session.getTransaction().commit();
+		session.close();
+	}
+	
+	public void modificarAbono(Abono f){
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.update(f);
+		session.flush();
+		session.getTransaction().commit();
+		session.close();
+	}
 
 }
--- Integracion/src/bean/srv/AbonoSRV.java
+++ Integracion/src/bean/srv/AbonoSRV.java
@@ -1,5 +1,31 @@
 package bean.srv;
 
-public class AbonoSRV {
+import java.util.List;
 
+import bean.Abono;
+import bean.dao.HibernateAbonoDAO;
+import bean.dao.HibernatePersonaDAO;
+
+public class AbonoSRV {
+	private static HibernateAbonoDAO dao;
+	static {
+		dao = HibernateAbonoDAO.getInstancia();
+	}
+	
+	public void grabarAbonos(List<Abono> Abonos){
+		dao.grabarAbonos(Abonos);
+	}
+	
+	public void borrarPersona(Abono p){
+		dao.borrarAbono(p);
+	}
+	
+	public Abono seleccionarPersona(int c){
+		return dao.seleccionarAbono(c);
+	}
+	
+	public void modificarAbono(Abono p){
+		dao.modificarAbono(p);
+		
+	}
 }
